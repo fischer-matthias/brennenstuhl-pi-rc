c830c1e455a182736443a0cce197143e0859144d
m@tthias.space <m@tthias.space>
2019-01-29 01:36:14 +0100

    add layout

--- app/src/main/java/work/matse/brennstuhl_pirc/MainActivity.kt
+++ app/src/main/java/work/matse/brennstuhl_pirc/MainActivity.kt
@@ -2,11 +2,20 @@ package work.matse.brennstuhl_pirc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import work.matse.brennstuhl_pirc.helper.IPMaskTextWatcher
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initIPRegex()
+    }
+
+    fun initIPRegex() {
+        val ip = findViewById<EditText>(R.id.iIP);
+        ip.addTextChangedListener(IPMaskTextWatcher())
     }
 }
