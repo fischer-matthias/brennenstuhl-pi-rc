7495779bd7685a0bab3b8e8bd98fd65b5f0f51ed
m@tthias.space <m@tthias.space>
2019-01-29 01:35:07 +0100

    add ipregex

---
+++ app/src/main/java/work/matse/brennstuhl_pirc/helper/IPMaskTextWatcher.kt
@@ -0,0 +1,27 @@
+package work.matse.brennstuhl_pirc.helper
+
+import android.text.Editable
+import android.text.TextWatcher
+import java.util.regex.Pattern
+
+class IPMaskTextWatcher() : TextWatcher {
+
+    private val regex = Pattern.compile("^((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])\\.){0,3}((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])){0,1}$")
+    private var previousText: String = ""
+
+    override fun afterTextChanged(s: Editable) {
+        if (regex.matcher(s).matches()) {
+            previousText = s.toString()
+
+            if (previousText.length == 3 || previousText.length == 7) {
+                s.append(".")
+            }
+        } else {
+            s.replace(0, s.length, previousText)
+        }
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+}
